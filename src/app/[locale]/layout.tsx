d4bd8bb8a151034d754da219992d5fecf49ec288
Carlos Gallaga <carlos.gallagav@gmail.com>
2024-09-19 15:23:48 -0500

    fix: deleted all the static functions

--- src/app/[locale]/layout.tsx
+++ src/app/[locale]/layout.tsx
@@ -55,10 +55,6 @@ type RootLayoutProps = {
   };
 }
 
-export function generateStaticParams() {
-  return routing.locales.map((locale) => ({ locale }))
-}
-
 export default async function RootLayout({ children, params: { locale } }: RootLayoutProps) {
   const messages = await getMessages();
 
