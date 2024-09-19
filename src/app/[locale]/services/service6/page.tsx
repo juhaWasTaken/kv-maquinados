278a1868e5547555ea1cb27321e7097f7e9638b6
Carlos Gallaga <carlos.gallagav@gmail.com>
2024-09-19 14:53:56 -0500

    fix: didnt import correct package from next-intl

--- src/app/[locale]/services/service6/page.tsx
+++ src/app/[locale]/services/service6/page.tsx
@@ -13,6 +13,7 @@ import { Plus } from "lucide-react";
 import parallaxContact from "@/assets/images/servicesHeader.webp";
 import image3 from "@/assets/images/image3.webp";
 import Image from "next/image";
+import { unstable_setRequestLocale } from "next-intl/server";
 
 const rajdhani = Rajdhani({ subsets: ["latin"], weight: '700' });
 interface Params {
